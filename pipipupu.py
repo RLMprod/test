d6b086847368810990aa92498b721ff4042628b8
RLMprod <roman.ulymov@gmail.com>
2023-08-24 21:32:41 +0300

    the job is done

--- pipipupu.py
+++ pipipupu.py
@@ -1,2 +1,13 @@
+from random import randint
 a = 2-1
 print(a-1)
+
+def love_dicks():
+    b = randint(0,3)
+    print(b)
+    if b == 0:
+        print(f'wow you found {b} dicks in your mouth, impressive')
+    else:
+        print(f'ha-ha you sucked {b} dicks you are a dick-sucker AHAHHAHAHAHAHH')
+
+love_dicks()
